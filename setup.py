3bf3eeacbd0fb3cc8569e2d6988a6bad6adb80d1
Spike^ekipS <spikeekips@gmail.com>
2013-07-10 16:18:52 +0900

    wrote `setup.py`

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+        name = 'twisted-memcached',
+        version = '0.1',
+        url = 'https://github.com/dustin/twisted-memcached',
+        description = 'memcached protocol in twisted',
+        packages = find_packages('.'),
+        package_dir = {'': '.' },
+        install_requires = (
+                'Twisted',
+            ),
+        zip_safe=False,
+    )
+
+
